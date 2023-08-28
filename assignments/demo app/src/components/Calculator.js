93accbe0eec59725eef8741daf3c6d6cd33beb4e
Giddaluru Shamanth <126405443+Shamanthsunny@users.noreply.github.com>
2023-08-28 17:51:09 +0530

    Add files via upload

---
+++ assignments/demo app/src/components/Calculator.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Add from './Add'
+import Subtraction from './Subtraction'
+import Multiply from './Multiply'
+const Calculator=()=>{
+
+
+return ( 
+    <>
+    <ul>
+        <li><Add/></li>
+        <li><Subtraction/></li>
+        <li><Multiply/></li>
+    </ul>
+    </>
+
+    
+)
+}
+export default Calculator
